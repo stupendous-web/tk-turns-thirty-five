df54afcf9661a7c92fe285b355f72df3506a9664
stupendous-web <topher@stupendousweb.com>
2023-06-04 11:02:58 -0700

    Think twice, code once.

--- pages/index.js
+++ pages/index.js
@@ -1,4 +1,5 @@
 import Head from "next/head";
+import { Box, Container, Heading, SimpleGrid } from "@chakra-ui/react";
 
 export default function Home() {
   return (
@@ -7,7 +8,27 @@ export default function Home() {
         <title>TK Turns 35!</title>
         <meta name="viewport" content="width=device-width, initial-scale=1" />
       </Head>
-      <main>hi terry</main>
+      <main>
+        <Container maxW={"container.sm"} pt={16}>
+          <Heading color={"primary.500"} textAlign={"center"}>
+            TK Turns 35!
+          </Heading>
+          <SimpleGrid columns={2} spacing={4}>
+            <Box>
+              Lorem ipsum dolor sit amet, consectetur adipisicing elit. A at
+              cupiditate eum, eveniet incidunt laudantium non numquam omnis
+              possimus qui quia quod reiciendis rem, saepe sapiente velit,
+              voluptatibus voluptatum? Soluta.
+            </Box>
+            <Box>
+              Lorem ipsum dolor sit amet, consectetur adipisicing elit. A at
+              cupiditate eum, eveniet incidunt laudantium non numquam omnis
+              possimus qui quia quod reiciendis rem, saepe sapiente velit,
+              voluptatibus voluptatum? Soluta.
+            </Box>
+          </SimpleGrid>
+        </Container>
+      </main>
     </>
   );
 }
